a55713834ddb5cc4f8d99ae6a3f00170dbae9514
JOHN BRIDGES <103251070+jbridges373@users.noreply.github.com>
2026-09-03 16:38:18 +0100

    Update download_from_url.py

--- Day 10 - Files - Create, Read & Download/download_from_url.py
+++ Day 10 - Files - Create, Read & Download/download_from_url.py
@@ -2,6 +2,22 @@ import os
 import requests
 import shutil
 from download_util import download_file
+url = "https://wikimedia.org"
+
+# Add a User-Agent header to mimic a real web browser
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+
+# # Pass the headers into the get request
+# r = requests.get(url, headers=headers)
+
+# # This will no longer throw a 403 error
+# r.raise_for_status() 
+
+print("Download successful!")
+
+
 THIS_FILE_PATH = os.path.abspath(__file__)
 BASE_DIR = os.path.dirname(THIS_FILE_PATH)
 DOWNLOADS_DIR = os.path.join(BASE_DIR, "downloads")
@@ -11,7 +27,7 @@ downloaded_img_path = os.path.join(DOWNLOADS_DIR, '1.jpg')
 url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Classic_view_of_a_cloudfree_Peyto_Lake%2C_Banff_National_Park%2C_Alberta%2C_Canada_%284110933448%29.jpg/330px-Classic_view_of_a_cloudfree_Peyto_Lake%2C_Banff_National_Park%2C_Alberta%2C_Canada_%284110933448%29.jpg"
 
 # a smallish item
-r = requests.get(url, stream=True)
+r = requests.get(url, headers=headers, stream=True)
 r.raise_for_status() # 200
 with open(downloaded_img_path, 'wb') as f:
     f.write(r.content)
